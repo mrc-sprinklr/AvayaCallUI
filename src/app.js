86825fb9cd696fa64cfa556f651f74d8a3b87bc6
mrc-sprinklr <mayukh.raychaudhuri@sprinklr.com>
2022-06-09 19:07:30 +0530

    socket

--- src/app.js
+++ src/app.js
@@ -1,16 +1,50 @@
 const JsSIP = require("JsSIP");
 
-const socket = new JsSIP.WebSocketInterface("wss://sample12.sip.zang.io:6065");
+const socket = new JsSIP.WebSocketInterface("wss://trunk.zang.io:5065");
+const configuration = {
+  sockets: [socket],
+  uri: "sip:sample12@sample12.sip.zang.io",
+  password: "sample12",
+};
 
-// const configuration = {
-//   sockets: [new JsSIP.WebSocketInterface(`wss://${sipDomain}:${sipPort}`)],
-//   uri: `sip:${sipNumber}@${sipDomain}`,
-//   authorization_user: `${sipNumber}`,
-//   password: `${sipNumber}`,
-//   registrar_server: `sip:${sipDomain}`,
-//   session_timers: false,
-//   connection_recovery_max_interval: CONNECTION_RECOVERY_MAX_INTERVAL,
-//   connection_recovery_min_interval: CONNECTION_RECOVERY_MIN_INTERVAL,
+const userAgent = new JsSIP.UA(configuration);
+
+userAgent.on("connected", function (e) {
+  console.log("connected");
+});
+
+userAgent.on("disconnected", function (e) {
+  console.log("disconnected");
+});
+
+userAgent.start();
+
+console.log("debug 1");
+
+// const options = {
+//   eventHandlers: {
+//     progress: function (e) {
+//       console.log("call is in progress");
+//     },
+//     failed: function (e) {
+//       console.log("call failed with cause: " + e.data.cause);
+//     },
+//     ended: function (e) {
+//       console.log("call ended with cause: " + e.data.cause);
+//     },
+//     confirmed: function (e) {
+//       console.log("call confirmed");
+//     },
+//   },
+//   mediaConstraints: { audio: true, video: false },
 // };
 
-// const userAgent = new JsSIP.UA(configuration);
+// let session = userAgent.call(
+//   "sip:sprinklr-sip@sprinklr.sip.zang.io",
+//   options
+// );
+
+let text1 = "testing...";
+userAgent.sendMessage("sip:sprinklr-sip@sprinklr.sip.zang.io", text1);
+
+console.log("debug 2");
